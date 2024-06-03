64ca3aeabc65ba32d772ddb7515fc51d4d8b6817
FreddyFunk <27208977+FreddyFunk@users.noreply.github.com>
2024-06-03 14:35:15 +0200

    init.sh: fix brew path on fresh installs

--- init.sh
+++ init.sh
@@ -5,6 +5,10 @@ set -o pipefail
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# add brew to path
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/$(whoami)/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # update brew
 brew update && brew upgrade
 
